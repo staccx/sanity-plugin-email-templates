296d3eeae8a79573cb1c5f352c49da0d34411aed
Espen Sæverud <espensaeverud@gmail.com>
2021-10-12 11:57:45 +0200

    fix: return object and not array

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export const allTemplatesQuery = `*[_type == "emailTemplate" && (!defined(namespace) || namespace == $namespace)]`;
-export const templateQuery = `*[_type == "emailTemplate" && (!defined(namespace) || namespace == $namespace) && slug.current == $slug]`;
+export const templateQuery = `*[_type == "emailTemplate" && (!defined(namespace) || namespace == $namespace) && slug.current == $slug][0]`;
