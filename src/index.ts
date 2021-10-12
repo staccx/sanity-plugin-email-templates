640d1d9eebe495cb2a5876aba3aac43cba112af5
Espen Sæverud <espensaeverud@gmail.com>
2021-10-12 14:47:42 +0200

    fix: update query to reflect removal of namespace

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export const allTemplatesQuery = `*[_type == "emailTemplate" && (!defined(namespace) || namespace == $namespace)]`;
-export const templateQuery = `*[_type == "emailTemplate" && (!defined(namespace) || namespace == $namespace) && slug.current == $slug][0]`;
+export const allTemplatesQuery = `*[_type == "emailTemplate"]`;
+export const templateQuery = `*[_type == "emailTemplate" && slug.current == $slug][0]`;
