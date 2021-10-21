7f4d2a4069f2c0f27ac78fac63bb28a281acb5b1
Rein Undheim <rein.undheim@protonmail.com>
2021-10-21 13:24:05 +0200

    feat(src): Renamed files to message instead of email. Added condionally rendered fields depending on if the message is an email or an sms

--- src/messageTemplate.ts
+++ src/messageTemplate.ts
@@ -1,7 +1,7 @@
 export default {
   type: "document",
-  name: "emailTemplate",
-  title: "Email Templates",
+  name: "messageTemplate",
+  title: "Message Templates",
   fields: [
     {
       type: "string",
@@ -17,15 +17,28 @@ export default {
       },
     },
     {
+      title: 'Is this message sent as an SMS?',
+      name: 'sms',
+      type: 'boolean'
+    },
+    {
       type: "text",
       name: "subject",
-      title: "Subject"
+      title: "Subject",
+      hidden: ({ parent }) => parent?.sms
     },
     {
       type: "array",
       name: "body",
       title: "Body",
       of: [{ type: "block" }],
+      hidden: ({ parent }) => parent?.sms
+    },
+    {
+      type: "text",
+      name: "text",
+      title: "Text message",
+      hidden: ({ parent }) => !parent?.sms
     },
   ],
 };
